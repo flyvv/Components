7520a86a93aa10fcc700f40e7189668f27413e63
flyvv <1003553948@qq.com>
2020-09-18 00:17:23 +0800

    fix: gh-page

--- .umirc.ts
+++ .umirc.ts
@@ -12,41 +12,43 @@ export default defineConfig({
           routes: [
             {
               path: '/list',
-              component: '../pages/home/List/index'
+              component: '../pages/home/List/index',
             },
             {
               path: '/imageList',
-              component: '../pages/home/ImageList/index'
+              component: '../pages/home/ImageList/index',
             },
             {
               path: '/tagList',
-              component: '../pages/home/TagList/index'
+              component: '../pages/home/TagList/index',
             },
             {
               path: '/ellipis',
-              component: '../pages/home/Ellipis/index'
+              component: '../pages/home/Ellipis/index',
             },
             {
               path: '/study',
-              component: '../pages/study/index'
+              component: '../pages/study/index',
             },
             {
-              path:'/expandTable',
-              component:'../pages/home/ExpandTable/index'
-            }
+              path: '/expandTable',
+              component: '../pages/home/ExpandTable/index',
+            },
           ],
         },
-
       ],
     },
   ],
   dva: {},
   antd: {},
+  base: '/Components/',
+  publicPath: '/Components/',
+  plugins: ['umi-plugin-gh-pages'],
   proxy: {
-    "/api/": {
-      "target": "https://pvp.qq.com/",
-      "changeOrigin": true,
-      "pathRewrite": { "^/api/": "" }
-    }
-  }
+    '/api/': {
+      target: 'https://pvp.qq.com/',
+      changeOrigin: true,
+      pathRewrite: { '^/api/': '' },
+    },
+  },
 });
